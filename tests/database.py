6a1f5259ffa89b41a0daff9e092deb22a7dae15f
AmauryMaros <maros.amaury@gmail.com>
2023-06-19 16:53:21 -0400

    postgres db test

--- tests/database.py
+++ tests/database.py
@@ -1,23 +1,27 @@
 from fastapi.testclient import TestClient
-from app.main import app
-from app.config import settings
+import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db, Base
-import pytest
+from app.main import app
 
-# creation of a new DB dependency to do our test on a test db & not on the dev db
-SQLACHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+from app.config import settings
+from app.database import get_db
+from app.database import Base
+from alembic import command
 
-engine = create_engine(SQLACHEMY_DATABASE_URL)
 
-TestingSessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
-#client = TestClient(app)
-# To create & destroy test db at every test :
 
-@pytest.fixture
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -27,9 +31,11 @@ def session():
     finally:
         db.close()
 
-@pytest.fixture
+
+@pytest.fixture()
 def client(session):
     def override_get_db():
+
         try:
             yield session
         finally:
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,44 +1,49 @@
-from app import schemas
-from .database import session, client
 import pytest
 from jose import jwt
+from app import schemas
+
 from app.config import settings
 
+
+# def test_root(client):
+
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Hello World'
+#     assert res.status_code == 200
+
+
 def test_create_user(client):
-    res = client.post("/users/",json = {"email":"test@gmail.com", "password":"test"})
+    res = client.post(
+        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "test@gmail.com"
+    assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
 
-def test_login_user(client, test_user):
-    res = client.post("/login", data = {"username": test_user["email"],             # in the request form, email is switched by username
-                                        "password": test_user["password"]})    
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
     login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token, 
-                         settings.secret_key, 
-                         algorithms=[settings.algorithm])
-    
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
     id = payload.get("user_id")
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
 
 
-@pytest.mark.parametrize("email, password, status_code", 
-                         [("wrongemail@gmail.com","test",403),
-                          ("test@gmail.com", "wrongpass", 403),
-                          ("wrongmail@gmail.com","wrongpass", 403),
-                          (None, "test", 422),
-                          ("test@gmail.com", None, 422)])
-
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)
+])
 def test_incorrect_login(test_user, client, email, password, status_code):
-    res = client.post("/login", data = {"username": email,
-                                        "password": password})
-    
-    assert res.status_code == status_code
+    res = client.post(
+        "/login", data={"username": email, "password": password})
 
-""" def test_root(client):
-    res = client.get("/")
-    print(res.json().get("message"))           # print(res) return Response 200 with pytest. To get the string "hello wordl", print(res.json())
-    assert res.json().get("message") == "Hello World"
-    assert res.status_code == 200 """
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
