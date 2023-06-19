ad05e52de37d7fb1d3bb532d84f908517425f433
AmauryMaros <maros.amaury@gmail.com>
2023-06-19 18:13:11 -0400

    resolving tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -3,7 +3,7 @@ from app import schemas
 
 
 def test_get_all_posts(authorized_client, test_posts):
-    res = authorized_client.get("/posts/")
+    res = authorized_client.get("/posts")
 
     def validate(post):
         return schemas.PostOut(**post)
