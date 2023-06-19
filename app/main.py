f2fc89fd54a38f4807c8e83fd6d62bb60f629616
AmauryMaros <maros.amaury@gmail.com>
2023-06-19 18:32:18 -0400

    pushing out to ubuntu

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World pushing out to Ubuntu"}
+    return {"message": "Hello World pushing out to Ubuntu!"}
 
 
 
